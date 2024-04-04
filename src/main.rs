e8d89184b561850425062721ee9a5b537bf98e30
Francesco Zucca <francescozucca30@gmail.com>
2024-04-04 23:26:53 +0200

    testing around with iced gui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
+use iced::widget::*;
+use iced::{Alignment, Element, Sandbox, Settings};
+
+fn main() -> iced::Result {
     println!("Hello, world!");
+
+    Counter::run(iced::Settings::default())
+}
+
+#[derive(Default)]
+struct Counter{
+    value: i32
+}
+
+#[derive(Copy, Debug, Clone)]
+pub enum Message{
+    Increment,
+    Decrement,
+}
+
+impl Sandbox for Counter{
+
+    type Message = Message;
+
+    fn new() -> Self{
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String{
+        String::from("Counter")
+    }
+
+    fn view(&self) -> Element<Message>{
+        crate::column![
+            button("+").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Message::Decrement),
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+
+    fn update(&mut self, message: Message){
+        match message{
+            Message::Increment => self.value += 1,
+            Message::Decrement => self.value -= 1,
+        }
+    }
 }
